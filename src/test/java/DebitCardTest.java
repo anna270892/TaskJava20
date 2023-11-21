da515b87c62ab9ca3082a9f1defacc580f45e40f
Anna Kochergina <akochergina92@gmail.com>
2023-11-21 20:19:29 +0300

    refactors25

--- src/test/java/DebitCardTest.java
+++ src/test/java/DebitCardTest.java
@@ -17,7 +17,7 @@ public class DebitCardTest {
     }
     @BeforeAll
     static void setUpAll() {
-        System.setProperty("webdriver.chrome.driver", "driver/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "C:/tmp/chromedriver.exe");
     }
 
     @BeforeEach
